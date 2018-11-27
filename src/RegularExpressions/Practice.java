7f0c613465cdf5d92c32604044ef591e85e78a27
AARaven <voronin.arty@gmail.com>
2018-11-27 17:15:00 +0400

    no message

--- src/RegularExpressions/Practice.java
+++ src/RegularExpressions/Practice.java
@@ -9,15 +9,16 @@ public class Practice {
 
         //ex1
         String firstEx = "Word cAT DATA A site Java tExt";
-        Pattern firstPattern = Pattern.compile("(\\p{Ll}{1}\\p{Lu}{2})|(\\p{Ll}{4})|(\\p{Ll}{1}\\p{Lu}{1}\\p{Ll}{2})");
+//      Pattern firstPattern = Pattern.compile("(\\p{Ll}{1}\\p{Lu}{2})|(\\p{Ll}{4})|(\\p{Ll}{1}\\p{Lu}{1}\\p{Ll}{2})");
+        Pattern firstPattern = Pattern.compile("(\\b[a-z]\\w+)");
         Matcher firstMatcher = firstPattern.matcher("Word cAT DATA A site Java tExt");
         System.out.println();
         System.out.println(firstMatcher.replaceAll(" ").replaceAll("\\s* "," "));
 
         //ex2
         String secondEx = "2057";//2013 - 5043
-        Pattern secondPattern = Pattern.compile("(^[2][0-9][1-9][3-9])|(^[3-4][0-9][0-9][0-9])|(^[5][0][0-4][0-3])");
-        Matcher secondMatcher = secondPattern.matcher("5043");
+        Pattern secondPattern = Pattern.compile("(^[2][0][1-9][3-9])|(^[3-4][0-9][0-9][0-9])|(^[5][0][0-4][0-3]|^[2][1-9][0-9][0-9])");
+        Matcher secondMatcher = secondPattern.matcher("2200");
         System.out.println(secondMatcher.replaceAll("This is a valid number"));
 
         //ex3
@@ -27,17 +28,16 @@ public class Practice {
         System.out.println(thirdMatcher.replaceAll(" ").trim());
 
         //ex4
-
         String fourthEx = "2018/10/09"; //DD/MM/YYYY;
         Pattern fourthPattern = Pattern.compile("^\\d{2}\\/\\d{2}\\/\\d{4}");
-        Matcher fourthMatcher = fourthPattern.matcher("12/12/2018");
+        Matcher fourthMatcher = fourthPattern.matcher("14/12/2018");
         System.out.println(fourthMatcher.find());
 
         //ex5
         String fifthEx = "Regular regular popular expressions entered popular use from 1968 1968 1968";
-        Pattern fifthPattern = Pattern.compile("(\\bregular\\b)|(\\d[19]68)");
+        Pattern fifthPattern = Pattern.compile("(\\b[r]\\w+.)");
         Matcher fifthMatcher = fifthPattern.matcher(fifthEx);
-        System.out.println(fifthMatcher.replaceAll("").replaceAll("  "," "));
+        System.out.println(fifthMatcher.replaceAll("*").replaceAll("  "," "));
 
         //ex6
         String sixthEx = "11122333";
@@ -47,7 +47,6 @@ public class Practice {
         System.out.println(sixthMatcher.replaceAll("*"));
 
         //ex7
-
         String seventhEx = "password wword word Heeeeeeello";
         Pattern seventhPattern = Pattern.compile("^$");
         Matcher seventhMatcher = seventhPattern.matcher(seventhEx);

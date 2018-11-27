9f4592b52e24147949c0de6a6b991ea74c6a7703
AARaven <voronin.arty@gmail.com>
2018-11-27 12:53:29 +0400

    pre pre pre pre alfa

--- src/RegularExpressions/Practice.java
+++ src/RegularExpressions/Practice.java
@@ -1,41 +1,65 @@
 package RegularExpressions;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Practice {
 
     public static void main(String[] args) {
 
-//        String firstPattern = "(\\p{Ll}\\p{Lu}\\w+)";
-//        Pattern patternFirst = Pattern.compile(firstPattern);
-//        Matcher firstMatcher = patternFirst.matcher(firstEx);
-//        System.out.println(firstMatcher.find());
-//
-//
-//        String thirdEx = "  Removing        Extra Spaces       Between Words in  the             text  ";
-//
-//
-//        Pattern pattern = Pattern.compile(firstEx);
-//        Matcher matcher = pattern.matcher(firstEx);
-//        System.out.println(matcher.find());
-//
-
+        //ex1
         String firstEx = "Word cAT DATA A site Java tExt";
-        System.out.println(firstEx);
-        System.out.println(firstEx.replaceAll("\\p{Ll}\\p{Lu}\\w+","").replaceAll("\\p{Ll}\\p{Ll}\\p{Ll}\\p{Ll}",""));
+        Pattern firstPattern = Pattern.compile("(\\p{Ll}{1}\\p{Lu}{2})|(\\p{Ll}{4})|(\\p{Ll}{1}\\p{Lu}{1}\\p{Ll}{2})");
+        Matcher firstMatcher = firstPattern.matcher("Word cAT DATA A site Java tExt");
+        System.out.println();
+        System.out.println(firstMatcher.replaceAll(" ").replaceAll("\\s* "," "));
 
-        String secondEx = "2450";
-        System.out.println(secondEx);
+        //ex2
+        String secondEx = "2057";//2013 - 5043
+        Pattern secondPattern = Pattern.compile("(^[2][0-9][1-9][3-9])|(^[3-4][0-9][0-9][0-9])|(^[5][0][0-4][0-3])");
+        Matcher secondMatcher = secondPattern.matcher("5043");
+        System.out.println(secondMatcher.replaceAll("This is a valid number"));
 
-        String thirdEx = "  Removing        Extra Spaces       Between Words in  the             text  ";
-        System.out.println(thirdEx.replaceAll("[\\s]+"," ").trim());
-        Pattern pattern = Pattern.compile("[\\s]+");
-        System.out.println(pattern.matcher(thirdEx).replaceAll(" ").trim());
+        //ex3
+        String thirdEx = "         Removing     Extra  Spaces         Between Words       in the              text   ";
+        Pattern thirdPattern = Pattern.compile("\\s* ");
+        Matcher thirdMatcher = thirdPattern.matcher(thirdEx);
+        System.out.println(thirdMatcher.replaceAll(" ").trim());
 
+        //ex4
 
+        String fourthEx = "2018/10/09"; //DD/MM/YYYY;
+        Pattern fourthPattern = Pattern.compile("^\\d{2}\\/\\d{2}\\/\\d{4}");
+        Matcher fourthMatcher = fourthPattern.matcher("12/12/2018");
+        System.out.println(fourthMatcher.find());
+
+        //ex5
         String fifthEx = "Regular regular popular expressions entered popular use from 1968 1968 1968";
-        System.out.println(fifthEx);
-        Pattern pattern5 = Pattern.compile("");
-        System.out.println(pattern5.matcher(fifthEx));
+        Pattern fifthPattern = Pattern.compile("(\\bregular\\b)|(\\d[19]68)");
+        Matcher fifthMatcher = fifthPattern.matcher(fifthEx);
+        System.out.println(fifthMatcher.replaceAll("").replaceAll("  "," "));
+
+        //ex6
+        String sixthEx = "11122333";
+//        Pattern sixthPattern = Pattern.compile("(\\d{2}(?!\\d))");
+        Pattern sixthPattern = Pattern.compile("");
+        Matcher sixthMatcher = sixthPattern.matcher(sixthEx);
+        System.out.println(sixthMatcher.replaceAll("*"));
+
+        //ex7
+
+        String seventhEx = "password wword word Heeeeeeello";
+        Pattern seventhPattern = Pattern.compile("^$");
+        Matcher seventhMatcher = seventhPattern.matcher(seventhEx);
+        System.out.println(seventhMatcher.replaceAll("*"));
+
+
+
+
+
+
+
+
+
     }
 }
